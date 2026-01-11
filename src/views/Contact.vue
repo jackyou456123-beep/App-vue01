5b485d6d2407cf7da20bad6102e4892ddd32942b
jackyou456123-beep <jackyou456123@gmail.com>
2026-01-11 16:39:41 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,34 @@
+<template >
+    <div>
+         <h1>Contact to Khemmaphong Songmutharak</h1>
+         <h3>Tel.038-743-690</h3>
+         <img alt="Vue logo" src="../assets/2.png">
+    </div>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
